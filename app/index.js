a22810165660788b2608e14f6000cc66e5f0b8b2
Truong-Huy Nguyen <truonghuy@gmail.com>
2016-12-10 23:57:33 -0600

    minor

--- app/index.js
+++ app/index.js
@@ -1,16 +1,19 @@
 var arDrone = require('ar-drone');
 var drone = arDrone.createClient();
-drone.takeoff();
+//
+// drone.takeoff();
+//
+// drone
+//   .after(1000, function() {
+//     drone.clockwise(1.0);
+//   })
+//   .after(1000, function() {
+//     drone.stop();
+//     drone.front(0.5);
+//   })
+//   .after(1000, function() {
+//     drone.stop();
+//     drone.land();
+//   })
 
-drone
-  .after(1000, function() {
-    drone.clockwise(1.0);
-  })
-  .after(1000, function() {
-    drone.stop();
-    drone.front(0.5);
-  })
-  .after(1000, function() {
-    drone.stop();
-    drone.land();
-  })
+drone.createRepl();
